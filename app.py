cd5828708d8523edf5429fdb42e8ee7fa7d2ee1a
Ruth Grace Wong <ruthgracewong@gmail.com>
2024-04-21 21:16:52 -0700

    fix emoji error preventing remaining emojis from being posted, increase to 5 emojis

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import tokens
 
 from openai import OpenAI
 import os
+import re
 # Use the package we installed
 from slack_bolt import App
 ai_client = OpenAI(
@@ -23,7 +24,7 @@ def emoji_react(client, event, logger):
         messages=[
           {
             "role": "system",
-            "content": "You are a intelligent assistant. You respond to all of the following messages with a single line representing four unique emojis, formatted for Slack. The emojis should represent things mentioned in the messages, with only zero or one emojis representing sentiment. Note that text surrounded by ~ or where the line starts with a negative emoji means that the task mentioned there was not completed - please exclude these lines from your emoji output. If there is an element of the text that is sad or stressed, please use the hug emoji to express comfort instead of something more specific for that part of the text. For example if someone's relative died please react with a hug instead of with an emoji representing the relative or death.",
+            "content": "You are a intelligent assistant. You respond to all of the following messages with a single line representing five unique emojis, formatted for Slack. The emojis should represent things mentioned in the messages, with only zero or one emojis representing sentiment. Note that text surrounded by ~ or where the line starts with a negative emoji like :no_pedestrians: means that the task mentioned there was not completed - please exclude these lines from your emoji output. If the messages express deep sadness or high stress, please use :people_hugging: to express comfort instead of something more specific for that part of the text. For example if someone's relative died please react with a hug instead of with an emoji representing the relative or death. Also, please use ungendered emojis, for example, :cook: is preferred over :female-cook: or :male-cook:",
           }, 
           {
             "role": "user",
@@ -34,14 +35,21 @@ def emoji_react(client, event, logger):
     )
     reply = chat_completion.choices[0].message.content
     print(f"{reply}")
-    app.client.reactions_add(
-      channel=event["channel"],
-      timestamp=event["ts"],
-      name="thumbsup",
-      token=tokens.bot_token,
-    )
+    reply = reply.strip().strip(":")
+    emojis = re.split(r':\s*:*', reply)
+    print(f"{emojis}")
   except Exception as e:
-    logger.error(f"Error publishing emoji reacts: {repr(e)}")
+    logger.error(f"Error getting emojis from OpenAI: {repr(e)}")
+  for emoji in emojis:
+    try:
+      app.client.reactions_add(
+        channel=event["channel"],
+        timestamp=event["ts"],
+        name=f"{emoji}",
+        token=tokens.bot_token,
+      )
+    except Exception as e:
+      logger.error(f"Error publishing {emoji} emoji react: {repr(e)}")
 
 # Ready? Start your app!
 if __name__ == "__main__":
