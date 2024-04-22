d761577d1851fc42163c35a62d41d1b49652ffd2
Ruth Grace Wong <ruthgracewong@gmail.com>
2024-04-21 20:26:27 -0700

    mess with prompt

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import tokens
+
+from openai import OpenAI
 import os
 # Use the package we installed
 from slack_bolt import App
+ai_client = OpenAI(
+    api_key=tokens.open_ai_key,
+)
 
 # Initialize your app with your bot token and signing secret
 app = App(
@@ -13,12 +18,26 @@ app = App(
 @app.event("message")
 def emoji_react(client, event, logger):
   try:
-     print(event["text"])
-     app.client.reactions_add(
-        channel=event["channel"],
-        timestamp=event["ts"],
-        name="thumbsup",
-        token=tokens.bot_token,
+    chat_completion = ai_client.chat.completions.create(
+        messages=[
+          {
+            "role": "system",
+            "content": "You are a intelligent assistant. You respond to the following messages with a single line representing four unique emojis, formatted for Slack. The emojis should represent things mentioned in the messages, with a focus on nouns and verbs, with one or no emojis representing a sentiment named in the message. If there is an element of the text that is sad or stressed, please use the hug emoji to express comfort instead of something more specific for that part of the text. For example if someone's relative died please react with a hug instead of with an emoji representing the relative or death.",
+          }, 
+          {
+            "role": "user",
+            "content": event["text"]
+          }, 
+        ],
+        model="gpt-3.5-turbo",
+    )
+    reply = chat_completion.choices[0].message.content
+    print(f"{reply}")
+    app.client.reactions_add(
+      channel=event["channel"],
+      timestamp=event["ts"],
+      name="thumbsup",
+      token=tokens.bot_token,
     )
   except Exception as e:
     logger.error(f"Error publishing emoji reacts: {repr(e)}")
