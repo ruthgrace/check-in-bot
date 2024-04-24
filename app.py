6f1830870ef748cb896d0cf11600d0e4db794e6c
Ruth Grace Wong <ruthgracewong@gmail.com>
2024-04-23 21:41:57 -0700

    try to ignore threaded replies

--- app.py
+++ app.py
@@ -18,38 +18,37 @@ app = App(
 # New functionality
 @app.event("message")
 def emoji_react(client, event, logger):
-  try:
-    print(event["text"])
-    chat_completion = ai_client.chat.completions.create(
-        messages=[
-          {
-            "role": "system",
-            "content": "You are a intelligent assistant. You respond to all of the following messages with a single line representing five unique emojis, formatted for Slack. The emojis should represent things mentioned in the messages, with only zero or one emojis representing sentiment. Note that text surrounded by ~ or where the line starts with a negative emoji like :no_pedestrians: means that the task mentioned there was not completed - please exclude these lines from your emoji output. If the messages express deep sadness or high stress, please use :people_hugging: to express comfort instead of something more specific for that part of the text. For example if someone's relative died please react with a hug instead of with an emoji representing the relative or death. Also, please use ungendered emojis, for example, :cook: is preferred over :female-cook: or :male-cook:",
-          }, 
-          {
-            "role": "user",
-            "content": event["text"]
-          }, 
-        ],
-        model="gpt-4",
-    )
-    reply = chat_completion.choices[0].message.content
-    print(f"{reply}")
-    reply = reply.strip().strip(":")
-    emojis = re.split(r':\s*:*', reply)
-    print(f"{emojis}")
-  except Exception as e:
-    logger.error(f"Error getting emojis from OpenAI: {repr(e)}")
-  for emoji in emojis:
+  if "thread_ts" not in event.keys():
     try:
-      app.client.reactions_add(
-        channel=event["channel"],
-        timestamp=event["ts"],
-        name=f"{emoji}",
-        token=tokens.bot_token,
+      chat_completion = ai_client.chat.completions.create(
+          messages=[
+            {
+              "role": "system",
+              "content": "You are a intelligent assistant. You respond to all of the following messages with a single line representing five unique emojis, formatted for Slack. The emojis should represent things mentioned in the messages, with only zero or one emojis representing sentiment. Note that text surrounded by ~ or where the line starts with a negative emoji like :no_pedestrians: means that the task mentioned there was not completed - please exclude these lines from your emoji output. If the messages express deep sadness or high stress, please use :people_hugging: to express comfort instead of something more specific for that part of the text. For example if someone's relative died please react with a hug instead of with an emoji representing the relative or death. Also, please use ungendered emojis, for example, :cook: is preferred over :female-cook: or :male-cook:",
+            }, 
+            {
+              "role": "user",
+              "content": event["text"]
+            }, 
+          ],
+          model="gpt-4",
       )
+      reply = chat_completion.choices[0].message.content
+      print(f"{reply}")
+      reply = reply.strip().strip(":")
+      emojis = re.split(r':\s*:*', reply)
     except Exception as e:
-      logger.error(f"Error publishing {emoji} emoji react: {repr(e)}")
+      logger.error(f"Error getting emojis from OpenAI: {repr(e)}")
+    for emoji in emojis:
+      try:
+        app.client.reactions_add(
+          channel=event["channel"],
+          timestamp=event["ts"],
+          name=f"{emoji}",
+          token=tokens.bot_token,
+        )
+      except Exception as e:
+        logger.error(f"Error publishing {emoji} emoji react: {repr(e)}")
 
 # Ready? Start your app!
 if __name__ == "__main__":
