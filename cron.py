ba81d026931a675a3c724c42fd1208492d4002e5
Ruth Grace Wong <ruthgracewong@gmail.com>
2025-04-03 05:07:44 +0000

    add cron.py with just outline of what the file should do

---
+++ cron.py
@@ -0,0 +1,21 @@
+
+
+# time-based check-in-bot tasks
+
+# ***Need to make sure actions are logged so that they can be reviewed.***
+
+# on the 25th of the previous month: announce signups for check in groups For the next month
+#    Also create the groups and add the check-in bot. 
+
+# on the last day of the previous month:
+#    Determine how many channels there should be (number of people // 12)
+#        Can adjust based on previous posting frequency information 
+#    Post a welcome message / intro thread in the channels, tagging everyone who should be in that channel
+#    Ensure that everybody is added.
+
+# on the 7th of the month:
+#    Message people who have not posted. 
+#    Message people who have only posted an intro and not posted a check-in to remind. 
+
+# on the 11th of the month: Remove people who have still not posted ever.
+#    Could also remove people who replied to the message asking to be removed even if they've already posted an intro.
